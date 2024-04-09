6370ca0fbace2406fa9ea1f84d07e355eea1b795
Dirk Lemmermann <dlemmermann@gmail.com>
2024-04-09 09:51:45 +0200

    Added support for scenic view.

--- pdfviewfx-demo/src/main/java/module-info.java
+++ pdfviewfx-demo/src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module com.dlsc.pdfviewfx.demo {
     requires com.dlsc.pdfviewfx;
+    requires org.scenicview.scenicview;
+    requires javafx.web; // due to scenic view
+    requires javafx.fxml; // due to scenic view
     requires fr.brouillard.oss.cssfx;
     exports com.dlsc.pdfviewfx.demo;
 }
